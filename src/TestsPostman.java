11f75d8ed23f184fa90b0a62c16cb8c4d134c966
Ilya Burov <ilya.burov.99@mail.ru>
2024-09-30 03:09:07 +0300

    Fix

--- src/TestsPostman.java
+++ src/TestsPostman.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -41,7 +42,7 @@ public class TestsPostman {
                 .body("headers.host", equalTo("postman-echo.com"))
                 .body("headers.x-forwarded-proto", equalTo("https"))
                 .body("headers.connection", equalTo("close"))
-                .body("url", equalTo("https://postman-echo.com/delete?item=12345"));  // Изменено на https
+                .body("url", equalTo("https://postman-echo.com/delete?item=12345"));
     }
 
     @Test
@@ -79,21 +80,19 @@ public class TestsPostman {
 
     @Test
     public void testPostData() {
-        String response = given()
-                .contentType("application/x-www-form-urlencoded")
-                .formParam("occupation", "Commander-in-Chief")
-                .formParam("year", "1861")
+        given()
+                .contentType("application/x-www-form-urlencoded; charset=utf-8")
+                .formParam("foo1", "bar1")
+                .formParam("foo2", "bar2")
                 .when()
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("form.occupation", equalTo("Commander-in-Chief"))
-                .body("form.year", equalTo("1861"))
-                .body("headers.host", equalTo("postman-echo.com"))
-                .body("headers.x-forwarded-proto", equalTo("https"))
-                .body("headers.connection", equalTo("close"))
-                .body("url", equalTo("https://postman-echo.com/post"))
-                .extract().response().asString();
+                .log().all()
+                .body("form.foo1", equalTo("bar1"))
+                .body("form.foo2", equalTo("bar2"));
+
+
     }
 
     @Test
