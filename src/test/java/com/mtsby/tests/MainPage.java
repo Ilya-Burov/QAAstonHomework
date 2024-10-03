2eb98dd21b35fba06ec8d7ea3bc5cbd3bb6144ff
Ilya Burov <ilya.burov.99@mail.ru>
2024-10-03 09:37:42 +0300

    Add task 18

---
+++ src/test/java/com/mtsby/tests/MainPage.java
@@ -0,0 +1,84 @@
+package com.mtsby.tests;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+public class MainPage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private By dropdownButton = By.cssSelector(".select__header");
+    private By cookieAcceptButton = By.id("cookie-agree");
+    private By blockTitleLocator = By.cssSelector(".pay__wrapper h2");
+    private By logoContainer = By.cssSelector(".pay__partners ul");
+    private By visaLogo = By.cssSelector("img[alt='Visa']");
+    private By mastercardLogo = By.cssSelector("img[alt='MasterCard']");
+    private By belkartLogo = By.cssSelector("img[alt='Белкарт']");
+    public MainPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+    }
+    public void acceptCookies() {
+        try {
+            WebElement cookieButton = wait.until(ExpectedConditions.elementToBeClickable(cookieAcceptButton));
+            cookieButton.click();
+        } catch (Exception e) {
+            System.out.println("Всплывающее окно cookie не появилось или уже было закрыто.");
+        }
+    }
+    private void clickUsingJavaScript(WebElement element) {
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+        jsExecutor.executeScript("arguments[0].click();", element);
+    }
+    public void openDropdown() {
+        wait.until(ExpectedConditions.elementToBeClickable(dropdownButton)).click();
+    }
+    public void selectServiceOption(String optionText) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30)); // Увеличено время ожидания
+        try {
+            // Ожидание видимости элемента
+            WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[text()='" + optionText + "']")));
+            // Ожидание кликабельности элемента
+            wait.until(ExpectedConditions.elementToBeClickable(element));
+            // Прокрутка к элементу для его отображения на экране
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+            // Пытаемся выполнить клик
+            element.click();
+        } catch (ElementNotInteractableException | TimeoutException e) {
+            System.out.println("Failed to click the element: " + e.getMessage());
+            // Если стандартный клик не удался, пробуем клик с помощью JavascriptExecutor
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("arguments[0].click();", driver.findElement(By.xpath("//p[text()='" + optionText + "']")));
+        }
+    }
+    public void selectInternetOption() {
+        selectServiceOption("Домашний интернет");
+    }
+    public void selectInstallmentOption() {
+        selectServiceOption("Рассрочка");
+    }
+    public void selectDebtOption() {
+        WebElement debtOption = driver.findElement(By.xpath("//p[text()='Задолженность']"));
+        // Прокрутка к элементу
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", debtOption);
+        // Клик по элементу с использованием JavascriptExecutor
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", debtOption);
+    }
+    public String getBlockTitle() {
+        WebElement blockElement = wait.until(ExpectedConditions.visibilityOfElementLocated(blockTitleLocator));
+        return blockElement.getText().replace("\n", " ").replace("\u00A0", " ").trim();
+    }
+    public boolean isVisaLogoDisplayed() {
+        WebElement logosElement = wait.until(ExpectedConditions.visibilityOfElementLocated(logoContainer));
+        return logosElement.findElement(visaLogo) != null;
+    }
+    public boolean isMastercardLogoDisplayed() {
+        WebElement logosElement = wait.until(ExpectedConditions.visibilityOfElementLocated(logoContainer));
+        return logosElement.findElement(mastercardLogo) != null;
+    }
+    public boolean isBelkartLogoDisplayed() {
+        WebElement logosElement = wait.until(ExpectedConditions.visibilityOfElementLocated(logoContainer));
+        return logosElement.findElement(belkartLogo) != null;
+    }
+}
---
+++ src/test/java/com/mtsby/tests/MtsTests.java
@@ -0,0 +1,143 @@
+package com.mtsby.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+@Epic("MTSBy Tests")
+public class MtsTests {
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private MainPage homePage;
+    private ReplenishmentPage paymentPage;
+
+    @BeforeEach
+    @Owner("Ilya Burov")
+    @DisplayName("Настройка браузера и инициализация страниц")
+    public void setUp() {
+        WebDriverManager.chromedriver().driverVersion("129.0.6668.71").setup();
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("https://mts.by");
+
+        homePage = new MainPage(driver);
+        paymentPage = new ReplenishmentPage(driver);
+    }
+    @Test
+    @Feature("Проверка названия блока")
+    @DisplayName("Проверка заголовка блока для выбранной услуги")
+    public void testCheckBlockTitle() {
+        homePage.acceptCookies();
+        homePage.openDropdown();
+        homePage.selectServiceOption("Услуги связи");
+
+        String actualBlockTitle = homePage.getBlockTitle();
+        String expectedBlockTitle = "Онлайн пополнение без комиссии";
+        assertEquals(expectedBlockTitle, actualBlockTitle, "Заголовок блока не совпадает.");
+    }
+    @Test
+    @Feature("Проверка логотипов")
+    @DisplayName("Проверка наличия логотипов платежных систем на странице")
+    public void testPaymentSystemLogos() {
+        homePage.acceptCookies();
+        homePage.openDropdown();
+        homePage.selectServiceOption("Услуги связи");
+
+        assertTrue(homePage.isVisaLogoDisplayed(), "Логотип Visa не найден.");
+        assertTrue(homePage.isMastercardLogoDisplayed(), "Логотип MasterCard не найден.");
+        assertTrue(homePage.isBelkartLogoDisplayed(), "Логотип Белкарт не найден.");
+    }
+    @Test
+    @Feature("Проверяем ссылку")
+    @DisplayName("Проверка работы ссылки 'Подробнее о сервисе'")
+    public void testMoreAboutServiceLink() {
+        homePage.acceptCookies();
+        homePage.openDropdown();
+        homePage.selectServiceOption("Услуги связи");
+
+        By moreInfoLink = By.xpath("//a[text()='Подробнее о сервисе']");
+        WebElement linkElement = wait.until(ExpectedConditions.elementToBeClickable(moreInfoLink));
+        String expectedUrl = "https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/";
+        linkElement.click();
+
+        String currentUrl = driver.getCurrentUrl();
+        assertEquals(expectedUrl, currentUrl, "URL после нажатия на ссылку не соответствует ожидаемому.");
+    }
+    @Test
+    @Feature("Проверка текста плейсхолдеров")
+    @DisplayName("Проверка плейсхолдеров в формах оплаты для всех услуг")
+    public void testCheckPlaceholdersForAllPaymentOptions() {
+        homePage.acceptCookies();
+
+        homePage.openDropdown();
+        homePage.selectServiceOption("Услуги связи");
+
+        paymentPage.scrollToElement(By.id("connection-phone"));
+
+        assertEquals("Номер телефона", paymentPage.getPhonePlaceholderText(), "Плейсхолдер для услуги связи неверен.");
+        assertEquals("Сумма", paymentPage.getSumPlaceholderText(), "Плейсхолдер для суммы неверен.");
+        assertEquals("E-mail для отправки чека", paymentPage.getEmailPlaceholderText(), "Плейсхолдер для email неверен.");
+        homePage.selectInternetOption();
+        paymentPage.scrollToElement(By.id("internet-phone"));
+        assertEquals("Номер абонента", paymentPage.getInternetPhonePlaceholderText(), "Плейсхолдер для домашнего интернета неверен.");
+        homePage.selectInstallmentOption();
+        paymentPage.scrollToElement(By.id("score-instalment"));
+        assertEquals("Номер счета на 44", paymentPage.getInstallmentPhonePlaceholderText(), "Плейсхолдер для рассрочки неверен.");
+        homePage.selectDebtOption();
+        paymentPage.scrollToElement(By.id("score-arrears"));
+        assertEquals("Номер счета на 2073", paymentPage.getDebtPhonePlaceholderText(), "Плейсхолдер для задолженности неверен.");
+    }
+    @Test
+    @Feature("Корректность заполнения формы")
+    @DisplayName("Проверка корректности заполнения онлайн-формы и отображения информации об оплате")
+    public void testOnlinePaymentForm() {
+        homePage.acceptCookies();
+        homePage.openDropdown();
+        homePage.selectServiceOption("Услуги связи");
+        String phoneNumber = "297777777";
+        String amount = "30";
+        paymentPage.fillPaymentForm(phoneNumber, amount, "test@mail.ru");
+        paymentPage.clickSubmitButton();
+        assertTrue(paymentPage.isPaymentDetailsWindowDisplayed(), "Окно подтверждения оплаты не отображается.");
+        paymentPage.switchToPaymentFrame();
+        assertEquals(formatPhoneNumber(phoneNumber), paymentPage.getDisplayedPhone(), "Номер телефона отображается неверно.");
+        assertEquals(formatAmount(amount), paymentPage.getDisplayedSum(), "Сумма отображается неверно.");
+        assertTrue(paymentPage.getPaymentButtonText().contains(amount), "Текст на кнопке неверен.");
+        assertTrue(paymentPage.isGooglePayButtonDisplayed(), "Кнопка Google Pay не отображается.");
+        assertTrue(paymentPage.isYandexPayButtonDisplayed(), "Кнопка Yandex Pay не отображается.");
+
+        paymentPage.switchToDefaultContent();
+    }
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+    private String formatPhoneNumber(String phoneNumber) {
+        return "Оплата: Услуги связи Номер:375" + phoneNumber;
+    }
+    private String formatAmount(String amount) {
+        if (!amount.contains(".")) {
+            amount += ".00";
+        }
+        return amount + " BYN";
+    }
+}
